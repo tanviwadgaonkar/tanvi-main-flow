7d1d313ed5c185ba6e4ff05ef3c508a566b36fae
tanviwadgaonkar <156992817+tanviwadgaonkar@users.noreply.github.com>
2024-04-09 21:03:32 +0530

    Add files via upload

---
+++ task 1.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+my_list=['apple','mango','banana']
+my_list.append('orange')
+my_list.remove('apple')
+my_list[0]='pumpkin'
+my_list
+
+
+# In[3]:
+
+
+my_dict={'andy':10,'John':20,'daniel':30}
+my_dict['marie']=40
+del my_dict['John']
+my_dict['mandy']=50
+my_dict    
+    
+
+
+# In[6]:
+
+
+my_set={10,20,30,40,50}
+my_set.add(60)
+my_set.remove(40)
+my_set.discard(10)
+my_set
+
+
+# In[ ]:
+
+
+
+
